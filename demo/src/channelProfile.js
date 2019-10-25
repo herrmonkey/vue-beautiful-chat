8951696a9c120dc9d6742eb91fc2fdf2bae77fee
Ricardo Gonzalez <rgonzalez@koherencia.com.co>
2019-10-25 14:55:34 -0500

    first commit

---
+++ demo/src/channelProfile.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: 'HYQ724',
+    name: 'HYQ724',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
+  },
+  {
+    id: 'MFY201',
+    name: 'MFY201',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
+  }
+]
---
+++ demo/src/chatProfiles.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: 'Oskie',
+    name: 'Oskie',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
+  },
+  {
+    id: 'Monkey',
+    name: 'Monkey',
+    imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
+  }
+]
